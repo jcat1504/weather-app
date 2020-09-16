c8153ca19b8324a42f7f615c10b10f71d6d4f6c0
Jessica Yin <jyin978@hotmail.com>
2020-09-16 16:38:40 -0700

    TEST PASSED! -

--- src/components/SearchBox/SearchBox.test.jsx
+++ src/components/SearchBox/SearchBox.test.jsx
@@ -1,30 +1,27 @@
 const puppeteer = require('puppeteer');
+
 describe('Searching', () => {
-    // beforeAll(async () => {
-    //     await page.goto('http://localhost:3000');
-    // })
-
     test('should search successfully', async () => {
-    const browser = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({headless: true})
     const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+    await page.click('input#searchBox')
+    await page.keyboard.type("");
+    await page.keyboard.press("Enter")
+    // await page.waitFor(2000)
 
-    await page.waitForSelector('#searchBox');
-    await page.focus('#searchBox');
+    // await page.type('');
+    // const searchValue = await page.$eval('#searchBox', el => el.value)
+    // console.log(searchValue);
 
-    await page.type('');
+    // await page.waitForSelector('#searchBox');
+    // await page.focus('#searchBox');
 
-    await page.press('Enter');
-    await page.click('.search-button');
-    await page.waitForNavigation();
 
-    browser.close();
+    // await page.press('Enter');
+    // await page.click('.search-button');
+    // await page.waitForNavigation();
+
+    await browser.close();
     })  
 })
-
-//await page.focus('input[name=searchBox]')
-// await page.keyboard.type('92804');
-// await page.keyboard.press("Enter");
-
-// const searchButton = await page.$('input[name=searchBox]');
-// await searchButton.click();
-
