811897381168f7faf5e303946809ec54bab3616e
Jessica Yin <jyin978@hotmail.com>
2020-09-17 16:48:56 -0700

    Create navBar.test.jsx

---
+++ src/components/Navbar/navBar.test.jsx
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 250,
+    devtools: true,
+  }
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+}
+
+describe('on page load', () => {
+  test(`contains city's name`, async() => {
+    let browser = await puppeteer.launch({})
+    let page = await browser.newPage()
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: ''
+    })
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.app-instruction');
+
+    const html = await page.$eval('.app-instruction', e => e.innerHTML);
+    expect(html).toBe('<p>Allow Location Access or type city name/zip code in search area to get started.</p>');
+    browser.close();
+  }
+   );
+  })
